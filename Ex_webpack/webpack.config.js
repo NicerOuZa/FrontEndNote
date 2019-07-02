b2e62ee3ecaa9e4bdb86ad06b27201ea65b11168
zt773503141 <42088736+zt773503141@users.noreply.github.com>
2019-07-02 20:59:30 +0800

    gx

---
+++ Ex_webpack/webpack.config.js
@@ -0,0 +1,15 @@
+
+let path = require('path')
+module.exports = {
+    // 模式 默认两种  production， development
+    mode:'development',
+    //入口
+    entry: './src/index.js',
+    output: {
+        // 打包后的文件名
+        filename: 'bundle.js',
+        // 路径必须是一个绝对路径 
+        //   使用path模块下的resolve方法将相对路径解析成绝对路径
+        path: path.resolve('./dist')
+    }
+}
---
+++ webpack.md
@@ -0,0 +1,32 @@
+# 一，安装和配置
+
+### 1，安装
+
+```js
+//全局安装
+npm install -g webpack
+//安装到你的项目目录
+npm install --save -dev webpack
+//安装webpack-cli
+npm install --save -dev webpack-cli
+```
+
+### 2，配置
+
+**0配置**
+
+```js
+-打包（支持js的模块化）
+npx webpack
+```
+
+**手动配置webpack**
+
+默认配置文件的名字  `webpack.config.js`
+
+```js
+-----webpack.config.js-----
+
+ 
+```
+
