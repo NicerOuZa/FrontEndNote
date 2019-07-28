81c0a85fe5b5a5f4bbd68b9af2dc75a6183c098d
zhangtao <773503141@qq.com>
2019-07-28 21:31:45 +0800

    更新

--- Ex-es6/main.js
+++ Ex-es6/main.js
@@ -1,3 +1,24 @@
-console.log("sad")
-console.log("sad")
-console.log("sad")
+
+var dog = null
+function Dog() {
+    this.name = "dog"
+    this.tem = null
+}
+function Par() {
+    this.name = 'Par'
+
+    this.say = () => {
+        console.log("say");
+        dog = new Dog()
+
+        dog.tem = () => {
+            console.log(this.name);
+        }
+    }
+}
+
+
+var a = new Par()
+a.say()
+
+dog.tem()
--- F1_react/todolist/src/App.js
+++ F1_react/todolist/src/App.js
@@ -1,14 +1,5 @@
 import React from 'react';
 
-
-class TodoList extends React.Component {
-  render(){
-    return (
-      <div>hhhhhh</div>
-    )
-  }
-}
-
 function App() {
   return (
     <div className="App">
